9e8a1a8c540cd481761f89054bf054580ce833e6
Sai Ganesh <saiganeshmutyala2005@gmail.com>
2025-06-16 22:49:16 +0530

    first commit

---
+++ frontend/src/pages/Contect.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Contect = () => {
+
+  return (
+    <div>
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+        <p>CONTACT <span>US</span> </p>
+      </div>
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm'>
+        <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='font-semibold text-lg text-gray-600'>OUR OFFICE</p>
+          <p className='text-gray-500'>57896 wills station,washington,USA</p>
+          <p className='text-gray-500'>Tel: (415) 555-0437 <br /> Email: saigane@gmail.com</p>
+          <p className='font-semibold text-lg text-gray-600'>Careers at HOSPO</p>
+          <p className='text-gray-600'>Learn more about our teams and job openings</p>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Contect
